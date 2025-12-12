aeb6da7fab2168782d2ee81c3898810b7b2326c5
Rahul <2ar.rahul@gmail.com>
2025-12-12 11:26:38 +0530

    removed  wrangler.toml

--- src/app/movies/[id]/page.jsx
+++ src/app/movies/[id]/page.jsx
@@ -68,7 +68,7 @@ export default function MovieDetailPage() {
   };
 
   // ---------------------------
-  // MAIN EFFECT
+  // MAIN EFFECT - Auth
   // ---------------------------
   useEffect(() => {
     if (!id) return;
